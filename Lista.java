4c40bf8aa8eb6d7ce257a3fa4d03d21584cc36a3
Christian <chrismcruiz@gmail.com>
2020-08-23 23:14:04 -0500

    doc lista y tarea

--- Lista.java
+++ Lista.java
@@ -9,39 +9,68 @@ import java.util.ArrayList;
 
 
 /**
- *
- * @author 8.1Pro
+ * Esta clase define objetos de tipo Tarea.
+ * 
+ * @authors Christian Ruiz Lagos, Kevin Joel Dlaikan
+ * @version 08/20/2020
  */
 public class Lista {
     //private Tarea tarea;
     private String nombre;
     ArrayList<Tarea> lista;
     
+     /**
+     * Constructor de la clase Lista.
+     * @param nombre El nombre de la Lista.
+     */
     public Lista(String nombre){
         this.nombre = nombre;
          lista = new ArrayList();
     }
 
+    /**
+     * Método que permite obtener el nombre de un objeto de tipo Lista.
+     * @return Nombre de la lista.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Método que permite establecer el nombre a un objeto de tipo Lista.
+     * @param nombre El nombre a asignar a la lista.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
     
+    /**
+     * Método que permite obtener la lista de Tareas contenidas en un objeto de tipo Lista.
+     * @return Lista que almacena objetos de tipo Tarea.
+     */
     public ArrayList<Tarea> getLista() {
         return lista;
     }
 
+    /**
+     * Método que permite establecer una lista que almacena Tareas a una instancia de tipo Lista.
+     * @param lista ArrayList de objetos tipo Tarea.
+     */
     public void setLista(ArrayList<Tarea> lista) {
         this.lista = lista;
     }
     
+    /**
+     * Método que permite agregar un objeto de tipo Tarea a un objeto de tipo Lista.
+     * @param tarea Objeto de tipo Tarea.
+     */
     public void agregarTarea(Tarea tarea){
         lista.add(tarea);
     }
     
+    /**
+     * Método que permite mostrar el nombre de las listas previamente creadas.
+     */
     public void mostrarLista(){
         lista.forEach((lista1) -> {
             System.out.println(lista1.getNombre());
--- Tarea.java
+++ Tarea.java
@@ -9,9 +9,12 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 /**
- *
- * @author 8.1Pro
+ * Esta clase define objetos de tipo Tarea.
+ * 
+ * @authors Christian Ruiz Lagos, Kevin Joel Dlaikan
+ * @version 08/20/2020
  */
 public class Tarea {
 
@@ -20,64 +23,117 @@ public class Tarea {
     private boolean favorita;
     private long tInicio = 0;
     private long tFinal = 0;
+    private long tInvertido = 0;
     private Lista lista;
     private Tarea tarea;
     private Gestor gestor;
     ArrayList<Lista> listas = new ArrayList();
     Scanner s = new Scanner(System.in);
-
+    
+     /**
+     * Constructor de la clase Tarea.
+     * @param nombre El nombre de la Tarea.
+     * @param fecha_entrega La fecha de entrega de la Tarea.
+     * @param favorita true si la Tarea es favorita o false si no lo es.
+     * @param lista La lista a la cual pertenece la Tarea.
+     */
     public Tarea(String nombre, LocalDate fecha_entrega, boolean favorita, Lista lista) {
         this.nombre = nombre;
         this.fecha_entrega = fecha_entrega;
         this.favorita = favorita;
         lista.agregarTarea(this);
     }
-
+    
+     /**
+     * Método que permite obtener el nombre de un objeto de tipo Tarea.
+     * @return Nombre de la tarea.
+     */
     public String getNombre() {
         return nombre;
     }
 
+     /**
+     * Método que permite obtener la fecha de entrega de un objeto de tipo Tarea.
+     * @return Fecha de entrega de la tarea.
+     */
     public LocalDate getFecha_entrega() {
         return fecha_entrega;
     }
 
+     /**
+     * Método que permite conocer si un objeto de tipo Tarea es favorito o no.
+     * @return true si la tarea es favorita o false si no lo es.
+     */
     public boolean isFavorita() {
         return favorita;
     }
 
+     /**
+     * Método que permite obtener la lista a la cual pertenece una instancia de tipo Tarea.
+     * @return Nombre de la lista destino.
+     */
     public Lista getListaDestino() {
         return lista;
     }
 
+     /**
+     * Método que permite establecer un nombre a un objeto de tipo Tarea.
+     * @param nombre Nombre de la Tarea.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+     /**
+     * Método que permite establecer una fecha de entrega a un objeto de tipo Tarea.
+     * @param fecha_entrega Fecha de entrega de la Tarea.
+     */
     public void setFecha_entrega(LocalDate fecha_entrega) {
         this.fecha_entrega = fecha_entrega;
     }
 
+     /**
+     * Método que permite establecer el estado del atributo favorito a un objeto de tipo Tarea.
+     * @param favorita Recibe true si la tarea es favorita o false si no lo es.
+     */
     public void setFavorita(boolean favorita) {
         this.favorita = favorita;
     }
 
+     /**
+     * Método que permite establecer la lista padre de un objeto de tipo Tarea.
+     * @param lista Recibe el nombre de la Lista a la cual pertenece.
+     */
     public void setListaDestino(Lista lista) {
         this.lista = lista;
     }
 
+     /**
+     * Método que permite almacenar el tiempo en nanosegundos al iniciar una Tarea.
+     */
     public void iniciarTarea() {
         tInicio = System.nanoTime();
     }
 
-    public void detenerTarea() {
+     /**
+     * Método que permite almacenar el tiempo en nanosegundos al detener una tarea. Además, acumula la resta del tiempo final - tiempo inicial.
+     */
+     public void detenerTarea() {
         tFinal = System.nanoTime();
+        tInvertido += tFinal - tInicio;
     }
-
+    
+     /**
+     * Método establece el atributo tiempo final a 0, y el tiempo inicial al tiempo actual en nanosegundos.
+     */
     public void reiniciarTarea() {
-        tInicio = System.nanoTime();
         tFinal = 0;
+        tInicio = System.nanoTime();
     }
 
+     /**
+     * Método de prueba para crear Lista desde la pestaña Tareas.
+     */
     public void crearLista() {
         while (true) {
             System.out.println("¿Qué nombre tendrá la lista? ");
@@ -93,6 +149,9 @@ public class Tarea {
         System.out.println();
     }
 
+     /**
+     * Método de prueba para crear Tarea.
+     */
     public void crearTarea() {
         while (true) {
             System.out.println("¿Cómo se llamará la tarea? (consejo: evita nombres genéricos como \"trabajo de...\" o \"cosa para entregar\") ");
@@ -132,6 +191,9 @@ public class Tarea {
         //l.mostrarLista();
     }
 
+     /**
+     * Método de prueba para crear Pestaña (menú que simula las funcionalidades de cada pestaña).
+     */
     public void pestana() {
         String resp;
         System.out.println("Improve v.0");
